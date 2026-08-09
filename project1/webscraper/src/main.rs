1c29d2bd4bf0d044904f231db0ddb918aa5c0d62
Hayley <hayleymccormack2028@u.northwestern.edu>
2026-08-09 17:50:13 -0400

    title and count print

--- project1/webscraper/src/main.rs
+++ project1/webscraper/src/main.rs
@@ -4,19 +4,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let body = reqwest::get("https://books.toscrape.com/").await?
     .text().await?;
 
-    let doc = Html::parse_document(&body);
+    let count = book_count(&body);
 
-    let selector = Selector::parse("p.price_color").unwrap();
+    println!("{count}");
 
-    for element in doc.select(&selector) 
-    {
-        println!("{}", element.inner_html());
-    }
+    let titles = title_fetch(&body);
+
+    println!("{titles}");
 
     Ok(())
 }
 
-pub fn book_count()
+pub fn book_count(body: &str) -> usize
 {
-    
+    let doc = Html::parse_document(&body);
+    let book_selector = Selector::parse("article.product_pod").unwrap();
+
+    doc.select(&book_selector).count()
+}
+
+pub fn title_fetch(body: &str) -> usize
+{
+    let doc = Html::parse_document(&body);
+    let title_selector = Selector::parse("h3 > a").unwrap();
+
+    for element in doc.select(&title_selector) 
+    {
+        let book_title = element.value().attr("title").unwrap_or("#");
+        println!("{book_title}");
+    }
+
+    return 1;
 }
